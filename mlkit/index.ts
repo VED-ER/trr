b1b059e5c25310b22d1ac3eef12afd90cffa8bfd
Vedran Erak <vedran1.erak1@gmail.com>
2024-07-10 00:11:48 +0200

    Delete unused types

--- mlkit/index.ts
+++ mlkit/index.ts
@@ -2,22 +2,6 @@ import { NativeModules } from "react-native"
 
 const { TextRecognitionModule } = NativeModules
 
-export type Rect = {
-    left: number
-    top: number
-    height: number
-    width: number
-}
-export type Line = {
-    text: string
-    rect: Rect
-}
-export type Block = {
-    text: string
-    rect: Rect
-    lines: Line[]
-}
-
 export const recognizeImage = (url: string): Promise<string> => {
     return TextRecognitionModule.recognizeImage(url)
 }
